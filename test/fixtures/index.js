246d6a693016a7bb53670c6a10a22e08c5df1896
rdpacheco <rdpacheco+github@gmail.com>
2015-09-08 10:45:53 -0700

    Finishing things up for the most part

---
+++ test/fixtures/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  ActivityRunner: require('./activity-runner'),
+  MongooseMock: require('./mongoose'),
+  SegmentMock: require('./segment'),
+  TestLogger: require('./logger'),
+  data: require('./data')
+};
